376ff6878eb038d40e0038fd652b32658af25920
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2025-07-25 01:58:02 +0300

    add JWT authentication and authorization filters with CORS configuration

---
+++ src/main/java/com/hikmetsuicmez/FoodApp/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.hikmetsuicmez.FoodApp.security;
+
+import com.hikmetsuicmez.FoodApp.auth_users.entity.User;
+import com.hikmetsuicmez.FoodApp.auth_users.repository.UserRepository;
+import com.hikmetsuicmez.FoodApp.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException("User not found with username: " + username));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
