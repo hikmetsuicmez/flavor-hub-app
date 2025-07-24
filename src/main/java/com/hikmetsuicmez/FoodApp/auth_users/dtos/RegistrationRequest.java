4feb91e2bceb9b1a2e54bf4a1192c1244c6e08b5
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2025-07-24 12:24:10 +0300

    add authentication DTOs for login and registration

---
+++ src/main/java/com/hikmetsuicmez/FoodApp/auth_users/dtos/RegistrationRequest.java
@@ -0,0 +1,32 @@
+package com.hikmetsuicmez.FoodApp.auth_users.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegistrationRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 4, message = "Password must be at least 4 characters long")
+    private String password;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    private List<String> roles;
+
+}
