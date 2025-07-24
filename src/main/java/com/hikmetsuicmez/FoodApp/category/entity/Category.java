c8d21611c1adb5c425d7eb76fc4f837bf3f12f9c
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2025-07-24 14:41:24 +0300

    add Category entity, DTO, and repository for category management

---
+++ src/main/java/com/hikmetsuicmez/FoodApp/category/entity/Category.java
@@ -0,0 +1,30 @@
+package com.hikmetsuicmez.FoodApp.category.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Menu> menus;
+}
